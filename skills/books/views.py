71401cb92ed3ff77811b262b5102d0d401114493
Jenn R <ada_wit11@hackademy.mx>
2022-08-23 12:35:06 -0500

    serializer

--- skills/books/views.py
+++ skills/books/views.py
@@ -5,6 +5,7 @@ from rest_framework.permissions import AllowAny
 
 from books.models import Book, Author
 
+from books.serializers import AuthorSerializer, BookSerializer
 
 # Create your views here.
 
@@ -12,37 +13,62 @@ class RetrieveBooks(APIView):
     permission_classes = (AllowAny, )
 
     def get(self, request):
-        books_list = Book.objects.all().values()
-        return Response(books_list, status=status.HTTP_200_OK) 
+        books_list = Book.objects.all()
+        serializer= BookSerializer(books_list, many=True)
+        return Response(serializer.data) 
 
 
 class RetrieveAuthors(APIView):
     permission_classes = (AllowAny, )
 
     def get(self, request):
-        author_list = Author.objects.all().values()
-        return Response(author_list, status=status.HTTP_200_OK) 
+        author_list = Author.objects.all()
+        serializer = AuthorSerializer(author_list, many=True)
+        return Response(serializer.data) 
+
+#sin serializador
+# class CreateAuthor(APIView):
+#     permission_classes = (AllowAny, )
+
+#     def post(self, request):
+#         author_obj = Author.objects.create(
+#             first_name = request.data.get('first_name',''),
+#             last_name = request.data.get('last_name',''),
+#             birth_date = request.data.get('birth_date',''),
+#         )
+#         return Response({'message':'Creado'}, status=status.HTTP_201_CREATED) 
 
 class CreateAuthor(APIView):
     permission_classes = (AllowAny, )
 
     def post(self, request):
-        author_obj = Author.objects.create(
-            first_name = request.data.get('first_name',''),
-            last_name = request.data.get('last_name',''),
-            birth_date = request.data.get('birth_date',''),
-        )
-        return Response({'message':'Creado'}, status=status.HTTP_201_CREATED) 
+        data = request.data
+        serializer = AuthorSerializer(data=data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED) 
 
+#sin serializador
+# class CreateBook(APIView):
+#     permission_classes = (AllowAny, )
+
+#     def post(self, request):
+#         book_obj = Book.objects.create(
+#             name = request.data.get('name',''),
+#             isbn = request.data.get('isbn',0),
+#             publisher_date = request.data.get('publisher_date','1700-01'),
+#             author_id = request.data.get('author_id',1)
+#         )
+#         return Response({'message':'Creado'}, status=status.HTTP_201_CREATED) 
 
 class CreateBook(APIView):
     permission_classes = (AllowAny, )
 
     def post(self, request):
-        book_obj = Book.objects.create(
-            name = request.data.get('name',''),
-            isbn = request.data.get('isbn',0),
-            publisher_date = request.data.get('publisher_date','1700-01'),
-            author_id = request.data.get('author_id',1)
-        )
-        return Response({'message':'Creado'}, status=status.HTTP_201_CREATED) 
+        data = request.data
+        serializer = BookSerializer(data=data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED) 
+
+    
